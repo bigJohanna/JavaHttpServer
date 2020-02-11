aa1af707955b3a73e46d2050a90f406e9b022985
SebastianWaltilla <49598221+SebastianWaltilla@users.noreply.github.com>
2020-02-11 09:03:09 +0100

    Update TheRequest.java

--- src/main/java/TheRequest.java
+++ src/main/java/TheRequest.java
@@ -1,6 +1,46 @@
 
 public class TheRequest {
 
+        String startLine;
+    HashMap<String, String> headers = new HashMap<String, String>();
+    String body;
+
+    public void TheRequest() {
+    }
+
+    public void setHeaders(String key, String value) {
+        this.headers.put(key, value);
+    }
+
+    public String getStartLine() {
+        return startLine;
+    }
+
+    public void setStartLine(String startLine) {
+        this.startLine = startLine;
+    }
+
+    public HashMap<String, String> getHeaders() {
+        return headers;
+    }
+
+        public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+
+
+
+
+
+
+
+    // OLD CODE
+    
     private String method;
     private String file;
     private String host;
