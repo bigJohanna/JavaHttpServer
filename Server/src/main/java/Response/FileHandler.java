fb3cb100b5c67c3056f3b0a1c3b85de13080a213
Patrik <pg.patrik@gmail.com>
2020-02-14 11:57:28 +0100

    starting to move methods to switch

---
+++ Server/src/main/java/Response/FileHandler.java
@@ -0,0 +1,12 @@
+package Response;
+
+import java.io.File;
+
+public class FileHandler {
+
+    public static final File WEB_ROOT = new File("Server/src/main/java/resources");
+    public static final String DEFAULT_FILE = "index.html";
+    public static final String FILE_NOT_FOUND = "404.html";
+    public static final String METHOD_NOT_SUPPORTED = "not_supported.html";
+
+}
